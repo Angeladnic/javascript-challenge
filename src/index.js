a429089b44e26ca1b374e450fe0717b8d928ae8e
Angela Daniela Chaparro <65191233+Angeladnic@users.noreply.github.com>
2020-07-14 16:27:41 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,11 +22,11 @@ fetchData(API + data1.results[0].id, (error2, data2) => {
    if (error2) return console.error(error1);
    console.log(`Segundo Llamado...`)
 fetchData(data2.origin.url, (error3, data3) => {
-      if (error3) return console.error(error3);
-      console.log(`Tercero Llamado...`)
-      console.log(`Personajes: ${data1.info.count}`);
-      console.log(`Primer Personaje: ${data2.name}`);
-      console.log(`Dimensión: ${data3.dimension}`);
+   if (error3) return console.error(error3);
+    console.log(`Tercero Llamado...`)
+    console.log(`Personajes: ${data1.info.count}`);
+    console.log(`Primer Personaje: ${data2.name}`);
+    console.log(`Dimensión: ${data3.dimension}`);
     });
   });
 });
